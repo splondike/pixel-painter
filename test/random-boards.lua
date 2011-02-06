1e7161e1cf1705038e48d974d308d30de6d776cf
Stefan Schneider-Kennedy <me@stefansk.name>
2011-02-06 13:02:19 +1100

    Added a statistics gathering script. Also a symmlink to pixel-painter in the tests directory.

---
+++ test/random-boards.lua
@@ -0,0 +1,20 @@
+#!/usr/bin/env lua
+
+as_library = true
+dofile("../pixel-painter.lua")
+
+difficulty = 3 --Hard
+for i=1,10000 do
+	dimension = diff_to_dimension(difficulty)
+	board = generate_board(dimension, i)
+	par = calculate_par(board)
+	print(par)
+end
+
+--state = {}
+--state["selected_colour"] = 1
+--state["par"] = par
+--state["move_number"] = 0
+--state["difficulty"] = difficulty
+--state["board"] = board
+--save_game(state, "board.save")
