9f802ea91ba7b324ba49f585deee71f5124e2e7e
Stefan Schneider-Kennedy <code@stefansk.name>
2013-05-01 21:16:01 -0300

    Extracted out tables_equal into its own file

--- test/test_floodfill.lua
+++ test/test_floodfill.lua
@@ -1,36 +1,5 @@
------
--- bool= Loc_CoveringContents( tbl1, tbl2 )
---
--- Returns 'true' if 'tbl1' covers all entries of 'tbl2' (with identical contents).
--- This means that 'tbl2' is a subset of 'tbl1' entries.
--- Modified from: http://lua-users.org/lists/lua-l/2003-04/msg00181.html
---
-local function Loc_CoveringContents( tbl1, tbl2 )
-    --
-    for k,v in pairs(tbl2) do
-        --
-        if (tbl1[k] ~= v) then
-            
-            if ((type(tbl1[k])~="table") or (type(v)~="table")) then
-                --
-                return false    -- some entry didn't exist or was different!
-            end
-            
-            -- Subtables need to be dived into (different refs doesn't mean
-            -- different contents).
-            --
-            if (not Loc_CoveringContents( tbl1[k], v )) then
-                return false
-            end
-            
-            -- go on...
-        end
-    end    
-    
-    return true     -- covered it all!
-end
-
 as_library = true
+dofile("util/test.lua")
 dofile("../pixel-painter.lua")
 
 test_board = {
@@ -48,7 +17,7 @@ expected_result = {
 }
 
 fill_board(test_board, 3, 1, 1, 1, non_matching)
-assert(Loc_CoveringContents(test_board, expected_result))
+assert(tables_equal(test_board, expected_result))
 
 test_board = {
 	{1, 2, 1, 2, 1},
@@ -67,4 +36,4 @@ expected_result = {
 }
 
 fill_board(test_board, 3, 1, 1, 1)
-assert(Loc_CoveringContents(test_board, expected_result))
+assert(tables_equal(test_board, expected_result))
---
+++ test/util/test.lua
@@ -0,0 +1,35 @@
+-- Utilities for tests
+
+-----
+-- bool= tables_equal( tbl1, tbl2 )
+--
+-- Returns 'true' if 'tbl1' covers all entries of 'tbl2' (with identical contents).
+-- This means that 'tbl2' is a subset of 'tbl1' entries.
+-- Modified from: http://lua-users.org/lists/lua-l/2003-04/msg00181.html
+--
+function tables_equal( tbl1, tbl2 )
+    --
+    for k,v in pairs(tbl2) do
+        --
+        if (tbl1[k] ~= v) then
+            
+            if ((type(tbl1[k])~="table") or (type(v)~="table")) then
+                --
+                return false    -- some entry didn't exist or was different!
+            end
+            
+            -- Subtables need to be dived into (different refs doesn't mean
+            -- different contents).
+            --
+            if (not tables_equal( tbl1[k], v )) then
+                return false
+            end
+            
+            -- go on...
+        end
+    end    
+    
+    return true     -- covered it all!
+end
+
+
