633449e3064e164245df14fb5cb6dd34ccd7fb17
Stefan Schneider-Kennedy <code@stefansk.name>
2013-05-08 12:26:23 -0300

    Added test for combining moves

--- test/test_undo_move.lua
+++ test/test_undo_move.lua
@@ -10,8 +10,22 @@ local board = {
 }
 graph_original = board_to_graph(board)
 
+-- Test that undoing a move works
 graph = board_to_graph(board)
 move = combine_nodes(graph, 1, 2)
 undo_move(graph, move)
-
 assert(tables_equal(graph_original, graph))
+
+-- Test that combining moves together works
+move1 = combine_nodes(graph, 1, 2)
+move2 = combine_nodes(graph, 1, 4)
+combined_move = combine_moves({move1, move2})
+undo_move(graph, combined_move)
+assert(tables_equal(graph_original, graph))
+
+-- Test that attempting to combine moves with a different
+-- remaining_node throws an exception
+move1 = combine_nodes(graph, 1, 2)
+move2 = combine_nodes(graph, 3, 4)
+status = pcall(combine_moves, {move1, move2})
+assert(status == false)
